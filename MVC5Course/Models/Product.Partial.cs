88fa0f7b1ea94c2b02cc0dbddd06fd4b302b2bc7
demon74527 <demon74527@gmail.com>
2017-05-06 17:02:11 +0800

    修改名稱
    修改名稱

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,8 +2,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
@@ -14,12 +15,18 @@ namespace MVC5Course.Models
         [Required]
         public int ProductId { get; set; }
         
+        [DisplayName("產品名")]
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         public string ProductName { get; set; }
 
+        [DisplayName("價格")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
         public Nullable<decimal> Price { get; set; }
+
+        [DisplayName("啟用")]
         public Nullable<bool> Active { get; set; }
+
+        [DisplayName("庫存")]
         public Nullable<decimal> Stock { get; set; }
     
         public virtual ICollection<OrderLine> OrderLine { get; set; }
